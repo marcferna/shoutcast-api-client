fe4af8f0d287e72c4c196f10ab29a491e38c3311
Marc Fernandez <marc.fernandezg@gmail.com>
2016-09-11 23:12:23 -0700

    Added test coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'shoutcast/api'
+require 'simplecov-json'
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::JSONFormatter,
+  ]
+)
+
+SimpleCov.start
